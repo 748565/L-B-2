db293012a98cae47b56c0c32035b9aaf8cdf1db0
748565 <31278546+748565@users.noreply.github.com>
2017-09-01 17:39:39 +0530

    Add files via upload

---
+++  Q5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main ()
+{
+float a,b,c;
+cout << " enter a no. ";
+cin >> a;
+cin >> b;
+c = pow(a,b);
+cout << " power of that no. " << c;
+return 0;
+}
