18c73e23e092b2e73355cf187ad668951c55d1ff
748565 <31278546+748565@users.noreply.github.com>
2017-09-01 17:41:20 +0530

    Add files via upload

---
+++ Q7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int a,b,c;
+cout << " enter angles of a triangle ";
+cin >> a;
+cin >> b;
+c = 180-(a+b);
+cout << " 3rd angle of the triangle " << c;
+return 0;
+} 
+
