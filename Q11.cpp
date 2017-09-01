cb4c6dd15ea684ccbd065201a229a247a8404f70
748565 <31278546+748565@users.noreply.github.com>
2017-09-01 17:43:36 +0530

    Add files via upload

---
+++ Q11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+float a,b,c,d;
+cout << " enter principle ";
+cin >> a;
+cout << " enter time ";
+cin >> b;
+cout << " enter rate of interest ";
+cin >> c;
+d = (a*b*c)/100;
+cout << " simple interest " << d;
+return 0;
+}
