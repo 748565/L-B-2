3d364f5ee0ac9f99721c98dfae3b74325c297828
748565 <31278546+748565@users.noreply.github.com>
2017-09-01 17:42:18 +0530

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+float a,b;
+cout << " enter temperature in fahrenheit ";
+cin >> a;
+b = (a-32)/1.8;
+cout << " temperature in celsius " << b;
+return 0;
+}
