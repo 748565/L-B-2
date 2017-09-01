9c7ef4e3d6b2e7c036735255708495246e64cc61
748565 <31278546+748565@users.noreply.github.com>
2017-09-01 17:42:42 +0530

    Add files via upload

---
+++ Q8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+float a,b,c;
+cout << " enter base and height of triangle ";
+cin >> a;
+cin >> b;
+c = (0.5*a*b);
+cout << " area " << c;
+return 0;
+}
