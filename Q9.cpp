4569c7cf2673d2128725152574661b10ad374421
748565 <31278546+748565@users.noreply.github.com>
2017-09-01 17:43:05 +0530

    Add files via upload

---
+++ Q9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+float a,b;
+cout << " enter side of eq. triangle ";
+cin >> a;
+b = 1.73/4*a*a;
+cout << " area of eq. triangle " << b;
+return 0;
+} 
