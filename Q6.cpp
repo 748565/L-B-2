cefdab7a5a05a2939e9b3185d0c7bf5682f3774b
748565 <31278546+748565@users.noreply.github.com>
2017-09-01 17:41:53 +0530

    Add files via upload

---
+++ Q6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main ()
+{
+float a,b;
+cout << " enter a no. ";
+cin >> a;
+b = pow(a,0.5);
+cout << " square root " << b;
+return 0;
+}
