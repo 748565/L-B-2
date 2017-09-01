42cbd727182607ffc99e88f911b4c651d2ea4a0c
748565 <31278546+748565@users.noreply.github.com>
2017-09-01 17:36:02 +0530

    Add files via upload

---
+++ Q4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout << " enter no. of days ";
+cin >> a;
+b = a/365 , c = a/7;
+cout << " years: " << b;
+cout << " weeks: " << c;
+return 0;
+}
